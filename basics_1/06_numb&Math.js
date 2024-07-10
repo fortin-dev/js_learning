c5df1c5a8d2179e683cdd4fc0816cebb7cef1b51
fortin-dev <abhiydv178@gmail.com>
2024-07-10 09:06:23 +0530

    first commit

---
+++ basics_1/06_numb&Math.js
@@ -0,0 +1,9 @@
+const num =107;
+// console.log(num);
+
+const score = new Number(50000);
+// console.log(score);
+// console.log(typeof score)
+
+console.log(score.toString().length);
+console.log(score.toFixed(1); 
