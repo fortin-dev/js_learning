3383f33fa9cb4e2df847e383e64eb4da1cefcdfb
fortin-dev <abhiydv178@gmail.com>
2024-07-10 12:19:39 +0530

    completed

--- basics_1/06_numb&Math.js
+++ basics_1/06_numb&Math.js
@@ -5,5 +5,31 @@ const score = new Number(50000);
 // console.log(score);
 // console.log(typeof score)
 
-console.log(score.toString().length);
-console.log(score.toFixed(1); 
+// console.log(score.toString().length);
+// console.log(score.toFixed(5)); 
+
+const flotNumber = 12345.987565;
+
+// console.log(flotNumber.toPrecision(3));
+ const hundrd = 1000000;
+//  console.log(hundrd.toLocaleString('en')); 
+//  console.log(hundrd.toLocaleString('en-IN')); 
+
+//.........................................................MATHS.........................................
+//********************************************************************************************************* */
+
+// console.log(Math.abs(-5));
+// console.log(Math.abs(5));
+// console.log(Math.round(5.6758));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(6.7));
+// console.log(Math.min(6,4,62,6,7,0));
+// console.log(Math.max(6,4,62,6,7,0));
+
+// console.log(Math.random());
+// console.log(Math.floor((Math.random()*10))+1);
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random() *(max- min)+1));
