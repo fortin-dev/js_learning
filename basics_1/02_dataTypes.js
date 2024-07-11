79c736c84b41767170665d91326861d7f441a83f
fortin-dev <abhiydv178@gmail.com>
2024-07-11 14:12:19 +0530

    other datatypes

--- basics_1/02_dataTypes.js
+++ basics_1/02_dataTypes.js
@@ -9,6 +9,39 @@ let nam = "Abhinav"
 let age = 18
 let earning = false
 
-console.log(typeof nam)
-vconsole.log(typeof age)
-console.log(typeof earning)
+// console.log(typeof nam)ṇ
+// vconsole.log(typeof age)
+// console.log(typeof earning)
+
+const intgr = 1000;
+const flot = 23.345;
+const strng = "abhinav";
+const booln = false;
+//  console.log(typeof intgr);
+//  console.log(typeof flot);
+//  console.log(typeof strng);
+//  console.log(typeof booln);
+
+const id =Symbol('123abc');
+const id2 =Symbol('123abc')
+
+// console.log(typeof id);
+// console.log(id === id2);
+
+// ............NON-PRIMITIVE
+
+const myObj = {
+    myName : "abhinav",
+    age : 19,
+}
+// console.log(myObj.myName);
+
+const myArray = ['abhinav', 19, 'bca'];
+// console.log(typeof myArray[1]);
+
+
+function myFunction(Name) {
+    console.log(Name);
+}
+
+// myFunction("abhinav");
---
+++ basics_1/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const myObj = {
+//     name = "abhinav",
+//     age : 19;
+// }
