25560429771f883fbd1fe6ecdd3b8f953745654c
fortin-dev <abhiydv178@gmail.com>
2024-07-16 13:39:06 +0530

    arrow functions

---
+++ basics_3/arrow_3.js
@@ -0,0 +1,49 @@
+// const userLogin ={
+//     username : "Abhinav",
+//     ign : 'fortin',
+//     message : function () {
+//         console.log(`${this.username} wellcome to game `);
+//         console.log(this);
+//     }
+// }
+
+// userLogin.message()
+// userLogin.username = "Yadav"
+// userLogin.message()
+
+// console.log(this);
+
+// function myWeb(){
+//     let nameuser = "Abhinav"
+//     console.log(this.nameuser)
+// }
+
+// const myWeb =function(){
+//     let nameuser = "Abhinav"
+//     console.log(this.nameuser)
+// }
+// myWeb() 
+
+// const myWeb = () => {
+//     let nameuser = "Abhinav"
+//     console.log(this.nameuser)
+// }
+const myWeb = () => {
+    let nameuser = "Abhinav"
+    console.log(this)
+}
+
+// myWeb()
+
+// const addNum = (num1, num2) => {
+//     return num1 +num2;
+// }
+
+// const addNum = (num1, num2) => num1 + num2
+// const addNum = (num1, num2) => (num1 + num2)
+
+const addNum = ()=> ({name : 'Abhinav'})
+
+//  console.log(addNum());
+ 
+
