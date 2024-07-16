77444389d63079591f1b88fe1576d7a4b4ccf733
fortin-dev <abhiydv178@gmail.com>
2024-07-16 18:04:36 +0530

    other loops

---
+++ iterations_5/loops_2.js
@@ -0,0 +1,25 @@
+//.............while loop..........
+
+let index =0;
+while (index < 10) {
+    // console.log("Index is ",index);
+    index++;
+}
+const Names = ['Abhinav', 'yadav', 'fortin','putin']
+
+let indx = 0;
+while (indx < Names.length) {
+    // console.log(`My Name is ${Names[indx]}`);
+    indx++;
+}
+
+//...............do-while loop...........
+let score =11;
+
+do{
+    console.log("score is ", score);
+    score++;
+}
+while (score <= 10);
+    
+
